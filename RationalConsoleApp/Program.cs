395bd37e9fb2bfcd279a06146ec8441aca645c9a
dominik1408 <79981778+dominik1408@users.noreply.github.com>
2022-03-25 19:02:37 +0100

    Add project files.

---
+++ RationalConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using RationalLib;
+using System;
+
+namespace RationalConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(default(int));
+            Console.WriteLine(default(string));
+            Console.WriteLine(default(BigRational));
+
+            Console.WriteLine(new BigRational(2 , 4));
+
+            var u = BigRational.Parse("3/4");
+            Console.WriteLine(u);
+        }
+    }
+}
---
+++ RationalLib/BigRationalEquals.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Numerics;
+
+namespace RationalLib
+{
+    public readonly partial struct BigRational : IEquatable<BigRational>
+    {
+        public bool Equals(BigRational other)
+        {
+            if (IsNaN(this) && IsNaN(other)) return true;
+            if (IsNaN(this) && !IsNaN(other) ||
+                IsNaN(other) && !IsNaN(this)
+                )
+                 return false;
+
+            return this.Numerator == other.Numerator && this.Denominator == other.Denominator;
+
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj is null) return false;
+            if (obj is BigRational other) return true;
+
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Numerator, Denominator);
+        }
+
+        public static bool operator ==(BigRational left, BigRational right)
+        {
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(BigRational left, BigRational right)
+        {
+            return !(left == right);
+        }
+    }
+}
